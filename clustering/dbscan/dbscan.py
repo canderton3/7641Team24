35e4f2ef5e363a65d5773657cfde8aa99d2685cc
darbycobb <darbycobb.ou.edu>
2020-10-29 17:25:54 -0400

    Updated DBSCAN

--- clustering/dbscan/dbscan.py
+++ clustering/dbscan/dbscan.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
 #sklearn imports
 from sklearn.cluster import DBSCAN
 from sklearn.decomposition import PCA #Principal Component Analysis
@@ -17,45 +17,72 @@ from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 def load_data():
     w_score = pd.read_csv(r"../../data/exoplanet_cleanedrf_w_score.csv").to_numpy()
     no_score = pd.read_csv(r"../../data/exoplanet_cleanedrf.csv").to_numpy()
-    return w_score, no_score
+    # Get Labels
+    labels = no_score[:, 0]
+    # Delete Labels from data
+    w_score = np.delete(w_score, 0, axis=1)
+    no_score = np.delete(no_score, 0, axis=1)
+    return w_score, no_score, labels
 
-def dbscan(data):
-    binary_vars = data[:, :4]
+def dbscan(X, y):
+    binary_vars = X[:, :4]
     print(binary_vars)
     # Scale non-binary variables
-    scaled_numeric = StandardScaler().fit_transform(data[: , 4:])
+    scaled_numeric = StandardScaler().fit_transform(X[: , 4:])
     # Recombine
     X = np.concatenate((binary_vars, scaled_numeric), axis = 1)
-    print(X)
 
     # Compute DBSCAN
-    db = DBSCAN(eps=0.3, min_samples=10).fit(X)
-    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-    core_samples_mask[db.core_sample_indices_] = True
+    db = DBSCAN(eps=0.7, min_samples=15).fit(X)
     labels = db.labels_
-
+    print("NEW LABELS: ", max(labels))
     # Number of clusters in labels, ignoring noise if present.
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
     n_noise_ = list(labels).count(-1)
 
     print('Estimated number of clusters: %d' % n_clusters_)
     print('Estimated number of noise points: %d' % n_noise_)
-    print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-    print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-    print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-    print("Adjusted Rand Index: %0.3f"
-        % metrics.adjusted_rand_score(labels_true, labels))
+    print("Homogeneity: %0.3f" % metrics.homogeneity_score(y, labels))
+    print("Completeness: %0.3f" % metrics.completeness_score(y, labels))
     print("Adjusted Mutual Information: %0.3f"
-        % metrics.adjusted_mutual_info_score(labels_true, labels))
+        % metrics.adjusted_mutual_info_score(y, labels))
     print("Silhouette Coefficient: %0.3f"
         % metrics.silhouette_score(X, labels))
 
+    # Find percent candidate / false positive in each cluster
+    for i in range (max(labels) + 1):
+        points_in_cluster = X[np.where(labels == i)]
+        points_in_cluster_label = y[np.where(labels == i)]
+        (unique, counts) = np.unique(points_in_cluster_label, return_counts=True)
+        frequencies = np.asarray((unique, counts)).T
+        print("CLUSTER ", i)
+        print(frequencies)
 
-def plot_dbscan():
-    pass
+
+    #plot_dbscan(X=X, y=y, dbscan_labels=labels)
+
+
+def plot_dbscan(X, y, dbscan_labels):
+    # Get two most important factors to plot
+    pca = PCA(n_components=2)
+    x = pca.fit_transform(X)
+    # Plot each cluster
+    num_clusters = max(dbscan_labels) + 1
+    for i in range(num_clusters):
+        points_in_cluster = x[np.where(dbscan_labels == i)]
+        labels = y[np.where(dbscan_labels == i)]
+        rgb = (np.random.random(), np.random.random(), np.random.random())
+        for j in range(points_in_cluster.shape[0]):
+            if (labels[j] == "CANDIDATE"):  
+                plt.scatter(points_in_cluster[j, 0], points_in_cluster[j, 1], c=[rgb], marker='*')
+            else:
+                plt.scatter(points_in_cluster[j, 0], points_in_cluster[j, 1], c=[rgb], marker=',')
+        print("CLUSTER ", i)
+    print("finished")
+    plt.show()
 
 if __name__ == "__main__":
-    w_score, no_score = load_data()
-    print(no_score[:,4:])
-    dbscan(no_score)
+    w_score, no_score, labels = load_data()
+    print(labels)
+    dbscan(no_score, labels)
 
