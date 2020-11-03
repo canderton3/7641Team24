ae1af6403ad07d74b2bf0b919eb3f504675fc664
darbycobb <darbycobb.ou.edu>
2020-11-03 18:03:37 -0500

    Update Feature Selection

--- clustering/dbscan/feature_selection.py
+++ clustering/dbscan/feature_selection.py
@@ -23,22 +23,29 @@ def load_data():
 
     # Drop Err columns
     exoplanet_data = exoplanet_data[exoplanet_data.columns.drop(list(exoplanet_data.filter(regex='err')))]
-    
+
     # Find columns with NaNs
     print("COLUMNS WITH NANS: {}, TOTAL NUMBER: {}".format(exoplanet_data.columns[exoplanet_data.isna().any()].tolist(), len(exoplanet_data.columns[exoplanet_data.isna().any()].tolist())))
     exoplanet_data.dropna(axis=0, inplace=True)
 
+    # Size after cleaning
+    print(exoplanet_data.columns.to_list())
+    print(len(exoplanet_data.columns.to_list()))
+    print(len(exoplanet_data))
     return exoplanet_data
 
 # Plot Pearson correlation with all variables
 def pearson_corr(df):
     pearson_df = df.corr(method="pearson")
+    ax = plt.axes()
     pearson_heatmap = sb.heatmap(pearson_df, 
             xticklabels=pearson_df.columns,
             yticklabels=pearson_df.columns,
             cmap='RdBu_r',
-            linewidth=0.5)
-    pearson_heatmap.get_figure().savefig('pearson.png')
+            linewidth=0.5,
+            ax = ax)
+    ax.set_title('Pearson Correlation Heatmap')
+    plt.show()
     '''
     #Correlation with output variable
     target = abs(pearson_df["koi_score"])
@@ -46,11 +53,6 @@ def pearson_corr(df):
     relevant_features = target[target>0.3]
     print("Extra Relevant Features: {}".format(relevant_features))
     '''
-
-
-# Recursive Feature Elimination
-def rfe(df):
-    pass
 
 # Select K Best
 def select_k_best(df):
@@ -82,12 +84,18 @@ def feat_importance(df):
 def rf(df):
     target = df['koi_pdisposition']
     feats = df.drop('koi_pdisposition', axis=1)
+    feats = feats.drop('koi_score', axis=1)
     rf_clf = RandomForestClassifier()
     rf_clf.fit(feats, target)
     feature_importance = rf_clf.feature_importances_
     most_important_features = feats.columns[np.argsort(rf_clf.feature_importances_)[::-1]]
     feat_importances = pd.Series(feature_importance, index=feats.columns)
-    feat_importances.nlargest(10).plot(kind='barh').get_figure().savefig("feature_importance_rf.png")
+
+    ax = plt.axes()
+    rf_importance_plt = feat_importances.nlargest(10).plot(kind='barh', ax = ax)
+    ax.set_title("Random Forest Feature Importance")
+    plt.show()
+    print("RF IMPORTANT FEATURES: ", feature_importance)
     print("MOST IMPORTANT FEATURES: ", most_important_features)
 
 # Output data with features selected
@@ -96,18 +104,13 @@ if __name__ == "__main__":
     print(df.head(20))
     print(df.shape)
     # Look at pearson correlation
-    #earson_corr(df)
-    # Select K Best features
-    select_k_best(df)
-    # Feature Importance
-
+    #pearson_corr(df)
+    # Random Forest Feature selection
+    rf(df)
     # Most important: koi_score, koi_fpflag_ss, koi_fpflag_nt, koi_fpflag_ec, koi_fpflag_co, koi_depth, koi_period, koi_teq, koi_model_snr 
     # Output CSV with most important features
-    with_score = df[["koi_pdisposition", "koi_score", "koi_fpflag_ss", "koi_fpflag_nt", "koi_fpflag_ec", "koi_fpflag_co", "koi_depth", "koi_period", "koi_teq", "koi_model_snr"]]
-    wo_score = df[["koi_pdisposition", "koi_fpflag_ss", "koi_fpflag_nt", "koi_fpflag_ec", "koi_fpflag_co", "koi_depth", "koi_period", "koi_teq", "koi_model_snr"]]
+    top_5 = df[["koi_pdisposition", "koi_fpflag_co", "koi_fpflag_nt", "koi_fpflag_ss", "koi_fpflag_ec", "koi_prad"]]
+    top_10 = df[["koi_pdisposition", "koi_fpflag_co", "koi_fpflag_nt", "koi_fpflag_ss", "koi_fpflag_ec", "koi_prad", "koi_depth", "koi_teq", "koi_impact", "koi_insol", "koi_model_snr"]]
 
-    feat_importance(wo_score)
-    rf(wo_score)
-
-    with_score.to_csv("../../data/exoplanet_cleanedrf_w_score.csv", index=False)
-    wo_score.to_csv("../../data/exoplanet_cleanedrf.csv", index=False)
+    top_5.to_csv("../../data/exoplanet_cleanedrf_top_5.csv", index=False)
+    top_10.to_csv("../../data/exoplanet_cleanedrf_top_10.csv", index=False)
--- midterm_report.md
+++ midterm_report.md
@@ -13,14 +13,18 @@ The dataset was sourced from Kaggle, linked [here](https://www.kaggle.com/nasa/k
 Thankfully the dataset came with a thorough data dictionary, linked [here](https://exoplanetarchive.ipac.caltech.edu/docs/API_kepcandidate_columns.html). Our goal was to use the possible exoplanet's physical characteristics to identify the object of interest as either a candidate or false positive, so we we focused mostly on the physical characteristics columns. More information about feature selection is below.
 
 ## Data Cleaning and Preparation
+This dataset was relatively easy to clean-there was no aggregation to be done or significant issues to handle. The main issues were NaN values and feature selection.
 ### Cleaning
 Before dropping any columns, 36 columns contained at least some NaN values. This obviously was not going to be helpful for our purposes, but we didn't necessarily want to just drop all 36 columns. We first removed essentially all columns that weren't physical characteristics of the object of interest, excepting the label koi_pdisposition. This includes the exoplanet's id and name(s).
 
 **Note:** The columns koi_pdispostion and koi_disposition are very similar. koi_disposition values are "the category of this KOI from the Exoplanet Archive", which includes CONFIRMED planets that have been verified manually by NASA. koi_pdisposition values are "the pipeline flag that designates the most probable physical explanation of the KOI". koi_pdisposition is therefore a better fit for our purposes, because we want to decrease the manual labor of NASA to identify candidate exoplanets and thus want to rely on the physical explanations rather than whether the object of interest is CONFIRMED in the Exoplanet Archive.
 
-After dropping the categorical columns, we still had the NaN problem.
+After dropping the categorical columns, we still had the NaN problem. There were two columns, koi_teq_err1 and koi_teq_err2 that had no values for any data points, so those were dropped. Then, we reached a spot in our data cleaning process where some decisions had to be made. Most of the continuous columns had err1 and err2 values, which were essentially the confidence interval range for the actual value measured for the column. For example, if koi_depth was 2, err1 could be 1, and err2 could be -0.5, telling us that the actual bounds for koi_depth were between [1.5, 3]. We considered having different columns for upper and lower bounds, but didn't see that the information added by that would be exceedingly helpful. Because of this we chose to essentially ignore all of the error columns and keep the measured values. After doing this, we removed the data points that still contained NaN values. We decided to not impute the missing values due to the variation between the physical characteristics of all of the data points.
+
+Cleaning the data of NaN values and unnecessary columns reduced our dataset to 22 columns and nearly 8000 data points. 8000 data points is plenty to use for our purposes. 
 
 ### Feature Selection Methods
+After cleaning the dataset, we were left with 22 features. Since we are focusing on physical characteristics, it is very probable that there are highly correlated variables. We checked this by visualizing the Pearson correlation between each feature. 
 
 
 # Methods
