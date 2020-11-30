8873811274a06107ba11932e09942dc4c612fe27
darbycobb <darbycobb@gatech.edu>
2020-11-30 09:33:23 -0600

    create skeleton for classification main class

---
+++ main_run/classification_main.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import itertools
+from mpl_toolkits.mplot3d import Axes3D
+#sklearn imports
+from sklearn.preprocessing import StandardScaler #used for 'Feature Scaling'
+from sklearn import metrics
+
+
+class MainMethods:
+    def load_data(self, X):
+        pass
+
+    def evaluate_classification_train(self, X, y):
+        pass
+
+    def evaluate_classification_test(self, X, y):
+        pass
+
+    def evaluation_plots(self, X, y):
+        pass
+
+class ClassificationModels:
+    def decision_tree(self, X, y):
+        pass
+
+    def log_regression(self, X, y):
+        pass
+
+    def naive_bayes(self, X, y):
+        pass
